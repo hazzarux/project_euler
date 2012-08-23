5cdccf2380a2f88bd10fb371cf519b902d551e62
Yigit Ozkan <yigitozkan2804@gmail.com>
2012-08-23 15:07:55 +0200

    final f'ing update

--- src/p7.py
+++ src/p7.py
@@ -3,21 +3,27 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 '''
 
-NUMBER = 10001
+NUMBER1 = 6
+NUMBER2 = 10001
+
+def is_prime(n):
+    '''check if integer n is a prime'''
+    # range starts with 2 and only needs to go up the squareroot of n
+    for x in range(2, int(n**0.5)+1):
+        if n % x == 0:
+            return False
+    return True
 
 
-def isPrime(p): #Fermat theorem!
-    if(p==2): return True
-    if(not(p&1)): return False
-    return pow(2,p-1,p)==1
-
-def generate_prime_number(n):
+def generate_nth_prime_number(n):
     currentCount = 1
     currentNumber = long(2)
+
     while currentCount < n:
         currentNumber += 1
-        if isPrime(currentNumber):
+        if is_prime(currentNumber):
             currentCount += 1
     return currentNumber
 
-print generate_prime_number(NUMBER)
+print generate_nth_prime_number(NUMBER1)
+print generate_nth_prime_number(NUMBER2)
