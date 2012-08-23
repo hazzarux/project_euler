a010b66767c1c430c92ea78b5f3a5caf87ff7830
Yigit Ozkan <yigitozkan2804@gmail.com>
2012-08-23 14:51:53 +0200

    update

--- src/p7.py
+++ src/p7.py
@@ -6,7 +6,7 @@ What is the 10 001st prime number?
 NUMBER = 10001
 
 
-def isPrime(p):
+def isPrime(p): #Fermat theorem!
     if(p==2): return True
     if(not(p&1)): return False
     return pow(2,p-1,p)==1
@@ -16,7 +16,7 @@ def generate_prime_number(n):
     currentNumber = long(2)
     while currentCount < n:
         currentNumber += 1
-        if isPrime( currentNumber ):
+        if isPrime(currentNumber):
             currentCount += 1
     return currentNumber
 
